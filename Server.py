d8929d7a4768617e8a1a53c3ff748f6d61b20531
Carbon <16768821+CarbonNeuron@users.noreply.github.com>
2018-11-28 17:56:04 -0600

    Added Server Logic

--- Server.py
+++ Server.py
@@ -1,6 +1,12 @@
 import websockets, asyncio
 async def server(websocket, path):
-    pass
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
 startserver = websockets.serve(server, 'localhost', 8765)
 asyncio.get_event_loop().run_until_complete(startserver)
 asyncio.get_event_loop().run_forever()
