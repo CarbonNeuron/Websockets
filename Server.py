2517b55fb736f32b4661143cfa9c45f519d5a082
Corbin <corbinowens5@gmail.com>
2018-11-28 15:51:55 -0800

    i imported websockets and asyncio

--- Server.py
+++ Server.py
@@ -1,1 +1,6 @@
 import websockets, asyncio
+async def server(websocket, path):
+    pass
+startserver = websockets.serve(server, 'localhost', 8765)
+asyncio.get_event_loop().run_until_complete(startserver)
+asyncio.get_event_loop().run_forever()
