dd313cb3ca33d4cd1bb1a5cac1c1bd0cd1b3aed6
Carbon <16768821+CarbonNeuron@users.noreply.github.com>
2018-11-28 18:10:26 -0600

    Updated client logic

--- Client.py
+++ Client.py
@@ -1,13 +1,6 @@
 import websockets, asyncio
 async def client():
-    async with websockets.connect(
-            'ws://localhost:8765') as websocket:
-        name = input("What's your name? ")
-
-        await websocket.send(name)
-        print(f"> {name}")
-
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
+    async with websockets.connect('ws://207.63.186.14:8080') as websocket:
+        pass
 
 asyncio.get_event_loop().run_until_complete(client())
